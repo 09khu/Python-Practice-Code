616ee5ed25222e4f8d4a29711de4a1349d8e6715
Khushbu Nemade <87029710+09khu@users.noreply.github.com>
2022-04-17 10:37:46 +0530

    Day1

---
+++ operators.py
@@ -0,0 +1,28 @@
+a = 34
+b = 35
+#arithmetic operator
+
+print("value of 34+35 is ",34+35)
+print("value of 34-35 is ",34-35)
+print("value of 34*35 is ",34*35)
+print("value of 34/35 is ",34/35)
+
+
+#assignment operator
+
+a = 34
+a += 2
+print(a)
+
+#comparison operartor
+
+b = (4>7)
+print(b)
+
+#logical operator
+
+bool1 = True
+bool2 = False
+print("value of bool1 and bool2 is",(bool1 and bool2))
+print("value of bool1 or bool2 is",(bool1 or bool2))
+print("value of not bool2 is",(not bool2))
