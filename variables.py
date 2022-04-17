091eab0086903b2d1efc2eda26c37ecdd0799d04
Khushbu Nemade <87029710+09khu@users.noreply.github.com>
2022-04-17 10:36:08 +0530

    Day 1

---
+++ variables.py
@@ -0,0 +1,21 @@
+
+a = "Khusbhu"    #container to store value
+b = 245          #keywords are reserved words like def , class import etc
+c = 34.78
+d = True
+e = None
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# printing the type of varaibles
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
