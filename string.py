cf65b2e081790f2c0ddad4d8f3ffa10613188677
Khushbu Nemade <87029710+09khu@users.noreply.github.com>
2022-04-18 17:06:43 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,13 @@
+a=input("Enter your name: ")
+print(a)
+#print(type(a))
+
+#Printing string by indexing
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+
+#Printing String by
+'''for i in a:
+    print(i)'''
