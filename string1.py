6a845c51480c2d849078ba28d16572ce7d0b0213
Khushbu Nemade <87029710+09khu@users.noreply.github.com>
2022-04-18 17:08:04 +0530

    Day2

---
+++ string1.py
@@ -0,0 +1,40 @@
+'''
+a = input("Enter a string : ")
+n =len(a)
+print("String in reverse order")
+s=""
+for i in range(n-1,-1,-1):
+    s+=a[i]
+
+print(s)   '''
+
+
+
+'''a = input("Enter a string : ")
+n = len(a)
+swapped=swap
+print("Print string through swapping:-")
+
+
+for i in range(0,n,1):
+    for j in range(n,0,-1):
+        swapped=swap(0,n)
+        print(swapped) '''
+
+
+
+'''str="hello world"[::-1]
+print(str)'''
+
+
+#For printing list in loop
+
+'''for i in range(1,11,1):
+    print(i)
+else:
+    print("Thank you!!")'''
+
+
+
+
+
