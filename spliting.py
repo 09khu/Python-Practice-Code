58bd37ceaca421c289b625350e615efecb907227
Khushbu Nemade <87029710+09khu@users.noreply.github.com>
2022-04-18 17:18:14 +0530

    Day2

--- spliting.py
+++ spliting.py
@@ -1,7 +1,7 @@
 #Program for spliting a string
 
 
-a = "Good Morning, Khushbu and manisha"
+a = "Good Morning, Khushbu and Manisha"
 y = a.split(" ")
 print("String before spliting: ", a) 
 print("String after spliting: ", y) 
