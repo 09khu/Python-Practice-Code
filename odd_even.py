928a857129edbb238df5771399b9a2507cb95502
Khushbu Nemade <87029710+09khu@users.noreply.github.com>
2022-04-19 08:55:24 +0530

    Day3

---
+++ odd_even.py
@@ -0,0 +1,52 @@
+
+#user defined program for even odd in floating numbers
+
+a = float(input("Enter a number"))
+x = a%2
+print(x)
+type(a)
+print(type(a))
+
+if (a==0):
+    print("Number is neither odd nor even")
+elif(a%2==0):
+    print("Numebr is even")
+else:
+    print("Numebr is odd")
+
+
+'''#Number is odd or even by taking values
+a=6
+if (a%2==0):
+    print("No is even")
+else:
+    print("No is odd")'''
+
+
+'''#user defined program
+
+a = int(input("Enter a number"))
+type(a)
+print(type(a))
+
+if (a%2==0):
+    print("No is even")
+else:
+    print("No is odd")'''
+
+
+
+'''#user defined program
+
+a = float(input("Enter a number"))
+type(a)
+print(type(a))
+
+if (a==0):
+    print("Number is neither odd nor even")
+elif(a%2==0):
+    print("Numebr is even")
+else:
+    print("Numebr is odd")'''
+
+
