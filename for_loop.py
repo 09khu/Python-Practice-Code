80d81bfab5445acc72b7b52ca8b70607f567b1be
Khushbu Nemade <87029710+09khu@users.noreply.github.com>
2022-04-19 09:00:48 +0530

    Day3

---
+++ for_loop.py
@@ -0,0 +1,39 @@
+
+
+#for loop
+
+'''a = 10
+for i in a:
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)'''
+
+#Printing numbers using range function
+
+for i in range(0,11):
+    print(i)
+
+'''a=5
+for i in range(a):
+    print(i)'''
+
+
+'''a=10
+for i in range(3,8):
+    print(i)
+
+print("\n")
+
+a=10
+for i in range(1,11,1):
+    print(i)
+
+print("\n")'''
+
