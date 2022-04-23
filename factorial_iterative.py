49c2c1e8a7a05020b1a2d2ea97161c59023d41b9
Khushbu Nemade <87029710+09khu@users.noreply.github.com>
2022-04-23 08:48:33 +0530

    Day6

--- factorial_iterative.py
+++ factorial_iterative.py
@@ -1,7 +1,7 @@
 n = int(input("Enter total number:"))
 r = int(input("Enter number of items to be arrange:"))
 
-#calculate factorial
+#calculate factorial of user defined number
 fact_n=1
 for i in range (1,n+1)
 fact_n *=i
