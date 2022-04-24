7cad97175706522926e1f99b1be4c6f93920841a
Khushbu Nemade <87029710+09khu@users.noreply.github.com>
2022-04-24 15:34:08 +0530

    Day8

---
+++ class.py
@@ -0,0 +1,11 @@
+class cars:
+	def __init__(self,b_name,model,types): #
+		for defining data
+		self.b_name=b_name
+		self.model=model
+		self.types=types
+
+#data variable
+car1=cars("marauti","caiz","sedan")	
+print(car1.model)
+
