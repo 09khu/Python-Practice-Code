ddfcb94e0ca90c77057cd3fc1b7b90d7f6feec9b
Khushbu Nemade <87029710+09khu@users.noreply.github.com>
2022-04-25 09:59:03 +0530

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,24 @@
+
+
+
+# Python program of stack implementation using list
+ 
+stack = []
+ 
+
+stack.append('k')
+stack.append('h')
+stack.append('u')
+ 
+print('Initial stack')
+print(stack)
+ 
+
+print('\nElements popped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+ 
+print('\nStack after elements are popped:')
+print(stack)
+ 
