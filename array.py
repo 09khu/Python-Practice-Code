bd2291d49077746de80c32d52a91fe52654d55be
Khushbu Nemade <87029710+09khu@users.noreply.github.com>
2022-04-26 07:34:27 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,15 @@
+from array import *
+
+#default one dimension array can be made
+x = array("I",[12,13,14],[15,16,17])
+print(x)
+print(type(x))
+
+#Third party library is made to used multidimensional array
+#python pacakge manager pip is need to be install to use numpy
+
+'''In jupiter'''
+import numpy as np
+x = np.array([[12,13,14],[15,16,17]])
+print(x)
+print(type(x))
